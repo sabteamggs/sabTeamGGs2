3f24b4db4bc3703c18180c8ded39649375f31251
sabteamggs <sabteam.org@gmail.com>
2026-07-01 13:07:01 +0200

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -24,9 +24,11 @@ export const botConfig = {
     activities: [
       {
         // Text users will see (example: "Playing /help | Titan Bot").
-        name: "Made with ❤️",
+        name: "#JUSTSABNOW
+          @sabTeamGGs
+        ",
         // Activity type number (0 = Playing).
-        type: 0,
+        type: 5,
       },
     ],
   },
